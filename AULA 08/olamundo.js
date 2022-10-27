45b98972327eb1ff972ccd1459c72b1a82feb6f0
tbmelo01 <batistamelo2012@hotmail.com>
2022-10-26 22:22:52 -0300

    aula onde tive a primeira aula de javaScript

---
+++ AULA 08/olamundo.js
@@ -0,0 +1,18 @@
+console.log("Olá Mundo")
+//console,log é um comando que vai exibir informações de consele de navegador.
+
+
+var variavel1 = 2;
+//var tem o tipo de escopo global
+var variavel2 = "Olá";
+//var tem o tipo de escopo local
+const variavel3 = 3 + 3;
+//const é de escopo global e não pode ser reescrita
+
+console.log("A Variável 1 é igual à ", variavel1)
+variavel1 = 4;
+console.log("Agora a Variável 1 é igual à ", variavel1)
+console.log("O Valor de Variável2 é igual à ", variavel2)
+variavel2 = 5;
+console.log("Agora a Variável2 é igual à ", variavel2)
+console.log("Essa é a constante ", variavel3)
